d2791bf38b27f952c9818e4c12861ef14ace78a2
Chris Okamuro <cokamuro@gmail.com>
2022-05-30 14:41:40 -0400

    static HTML changed to prototype row with jQuery cloning.  styles set in applyClasses

---
+++ script.js
@@ -0,0 +1,37 @@
+var elRoot = $("#root");
+var elTable = $("#time-block")
+var currentHour=13;
+
+function ApplyClasses(){
+    $('button').addClass("saveBtn");
+    $('textarea').addClass("description");
+
+    for(var i=9;i<=17;i++){
+        var dispTime;
+        if(i<12){
+            dispTime=i+"AM"
+        } else if(i==12) {
+            dispTime=i+"PM"
+        } else {
+            dispTime=(i-12)+"PM"
+        }
+        if(i!=9){$('tr').first().clone().appendTo($('tbody'));}
+        var thisRow=$('tr').last();
+        if(i!=9){thisRow.removeClass("event-9");}
+        thisRow.children().eq(0).text(dispTime);
+        thisRow.attr("id","event-"+i);
+        
+
+        if(i<currentHour){
+            thisRow.children().eq(1).addClass("past");
+        } else if (i==currentHour) {
+            thisRow.children().eq(1).addClass("present");
+        } else {
+            thisRow.children().eq(1).addClass("future");
+        }
+        thisRow.children().eq(0).addClass("hour");
+    }
+    
+}
+
+ApplyClasses();
